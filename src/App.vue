6ad1896c41d48c9745dac217e9f3eae1c01581df
张超\75738 <757382771@qq.com>
2023-09-17 15:57:27 +0800

    测试three的点击事件

--- src/App.vue
+++ src/App.vue
@@ -18,16 +18,34 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGL1Renderer()
 renderer.setSize(window.innerWidth,window.innerHeight)  // 渲染屏幕的大小，整个屏幕的宽高
 document.body.appendChild(renderer.domElement)
-
 // 创建几何体
 const geometry = new THREE.BoxGeometry(1,1,1);
+geometry.name ='box'
+geometry.uuid ='box'
+console.log(geometry);
 // 创建材质
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
 // 创建网格
 const cube = new THREE.Mesh(geometry,material)
-
 // 将网格添加到场景中
 scene.add(cube)
+
+// 点击事件
+const mouse=new THREE.Vector2()
+let rayCaster=new THREE.Raycaster()
+const handlerClick=(e)=>{
+    mouse.x=(e.clientX/window.innerWidth)*2-1  //通过web中鼠标坐标得到标准屏幕坐标
+    mouse.y=-(e.clientY/window.innerHeight)*2+1
+    rayCaster.setFromCamera(mouse,camera)   //通过摄像机和鼠标位置更新射线
+ 
+    let intersects=rayCaster.intersectObjects(scene.children) //获取场景中所有与射线相交的物体，结果是一个数组
+    if(intersects.length>0){ //length>0则说明有与射线相交的问题。表明点击了
+      intersects.forEach(item => {
+        id(item.object)
+      })
+    }
+}
+window.addEventListener("click",handlerClick)
 
 // 设置相机位置
 camera.position.z = 5;  // z是正对我们方向，x是横轴，y是垂直
@@ -40,25 +58,24 @@ scene.add(axesHelper)
 // 创建轨道控制器，可以控制相机，可以通过监听不同的dom的轨道控制器，进行各种操作
 const controls = new OrbitControls(camera, renderer.domElement);
 // 设置阻尼惯性
-controls.enableDamping = true
+// controls.enableDamping = true
 // 设置阻尼系数,默认0.05调小相当于没有阻碍，加大滑动
-controls.dampingFactor = 0.01
+// controls.dampingFactor = 0.01
 
 // 渲染函数
 function animate() {
   // 更新控件，这只阻尼惯性，需要执行更新函数
-  controls.update()
+  // controls.update()
   // 请求动画帧，会一帧一帧的调用动画函数
   requestAnimationFrame(animate)
 
-  // // 不停旋转
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
+  // 不停旋转
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
   // 不停的渲染
   renderer.render(scene,camera)
 }
 animate()
-// renderer.render(scene,camera)
 
 
 </script>
